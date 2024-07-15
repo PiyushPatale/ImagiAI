4a310f4abab105dd0e4ab3f1ae1dc81e94768c8f
PIYUSH PATALE <patale.bhausaheb21b@iiitg.ac.in>
2024-07-15 23:36:35 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+    </div>
+  )
+}
+
+export default Home
