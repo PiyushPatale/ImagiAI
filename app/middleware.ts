56096cf463e26c02c508509137d95daddce0e14a
PIYUSH PATALE <patale.bhausaheb21b@iiitg.ac.in>
2024-07-17 01:57:24 +0530

    implement clerk webhook

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,9 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  // // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api/webhooks/clerk'],
-  // ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/sign-in"]
+  publicRoutes: ['/api/webhooks/clerk'],
  
 
 });
