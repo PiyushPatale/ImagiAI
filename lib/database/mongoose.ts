699464dde09ffe15c1f9eab81a8143b5db30c80d
PIYUSH PATALE <patale.bhausaheb21b@iiitg.ac.in>
2024-07-17 02:32:29 +0530

    implement clerk webhook

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -21,7 +21,7 @@ export const connectToDatabase = async ()=>{
 
     if(!MONGODB_URL) throw new Error('MONGODB_URL is not defined');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {dbName: 'imaginifyAi', bufferCommands: false})
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {dbName: 'photoai', bufferCommands: false})
 
     cached.conn = await cached.promise;
 
