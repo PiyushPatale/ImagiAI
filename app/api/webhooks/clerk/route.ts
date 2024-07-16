5aa4bbca8d91b37ab8d72d99c18bd6138f988ef0
PIYUSH PATALE <patale.bhausaheb21b@iiitg.ac.in>
2024-07-17 01:54:53 +0530

    implement clerk webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,15 +1,13 @@
-// import { clerkClient } from "@clerk/nextjs";
+import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
-import { clerkClient } from "@clerk/clerk-sdk-node";
-
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET=process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -65,8 +63,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name ?? "",
-      lastName: last_name ?? "",
+      firstName: first_name,
+      lastName: last_name,
       photo: image_url,
     };
 
@@ -89,8 +87,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name ?? "",
-      lastName: last_name ?? "",
+      firstName: first_name,
+      lastName: last_name,
       username: username!,
       photo: image_url,
     };
