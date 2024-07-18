019e8440968d0cbef0ed5216aded7bebf4a8dd15
PIYUSH PATALE <patale.bhausaheb21b@iiitg.ac.in>
2024-07-18 11:58:59 +0530

    implement clerk webhook

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,4 +1,4 @@
-import { Schema, model, models } from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose"  ;
 
 const UserSchema = new Schema({
   clerkId: {
@@ -36,6 +36,6 @@ const UserSchema = new Schema({
   },
 });
 
-const User = models?.User || model("User", UserSchema);
+const User = mongoose.model("user", UserSchema);
 
 export default User;
